0d428c30c5de5924a29b001f2f7ab4a605c1d791
lumen1296 <lumen1996@gmail.com>
2021-11-26 15:57:13 -0500

    feat: folder structure and database connection added

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: false,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      schema: process.env.DATABASE_SCHEMA
+    }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
